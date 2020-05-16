ff7053b15257d794da5ed85614d30096df4b1f8a
hydride0 <erkiwi@mail.com>
2020-05-16 18:38:42 +0200

    Create ctf-aliases.sh

---
+++ ctf-aliases.sh
@@ -0,0 +1,21 @@
+# To use these aliases, functions and variables put
+# "source /path/to/cft-aliases.sh" to your shellrc file.
+# These are my personal preferences and they'll change and improve
+# with time. I'll try to keep this repo update.
+
+export IP=127.0.0.1 # to be set before before starting with a new machine
+export MACHINE_DIR=$HOME/machines
+export CURRENT_DIR=$MACHINE_DIR/$IP
+
+nmap_most() {
+  mkdir $CURRENT_DIR
+  nmap -A -v -T4 -sC -oN $CURRENT_DIR/$IP.scan -p- $IP
+}
+
+gobuster_dir() {  gobuster dir -w $1 -u http://$IP  }
+
+md5() {  echo $1 | md5sum - | awk '{ print $1 }'  }
+
+e4l() {  enum4linux -a $IP | grep -v "Use of uninitialized value" > $MACHINE_DIR/$IP/enum4linux_$IP  }
+
+alias httpserv="echo Your IP is $(ip --br addr|tail -n1|awk '{print $3}' | sed 's/\/..//g') && python3 -m http.server"
